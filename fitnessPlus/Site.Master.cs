dab35f2efd77dc0d6e39452be77c6230e9ad5d63
Ciaran McMullan <mc.ciaran94@googlemail.com>
2019-02-05 13:24:27 +0000

    firebase code removed from site.master.cs

--- fitnessPlus/Site.Master.cs
+++ fitnessPlus/Site.Master.cs
@@ -14,26 +14,9 @@ namespace fitnessPlus
     public partial class SiteMaster : MasterPage
     {
 
-        IFirebaseConfig config = new FirebaseConfig                             //configuring firesbase
-        {
-            AuthSecret = "J1199wPZPQISsn6yAb0yMA4TfjAMIHzktNkiK2jh",
-            BasePath = "https://fitnessplus-5d8b8.firebaseio.com/"
-        };
-
-        IFirebaseClient client;
-
         protected void Page_Load(object sender, EventArgs e)
         {
-            client = new FireSharp.FirebaseClient(config);
-
-            if (client != null)
-            {
-                lblfirebaseConnection.Text = "Firebase Connection Established";
-            }
-            else
-            {
-                lblfirebaseConnection.Text = "Connection Error!";
-            }
+           
 
         }
     }
